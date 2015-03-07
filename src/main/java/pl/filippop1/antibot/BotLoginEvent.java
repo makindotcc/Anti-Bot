026e1c3b8bfcd9534860d37b4245b4421640501c
filippop1 <sheep3395@gmail.com>
2015-03-07 18:03:38 +0100

    BotLoginEvent

---
+++ src/main/java/pl/filippop1/antibot/BotLoginEvent.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 Filip.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.filippop1.antibot;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class BotLoginEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    private final BotPlayer player;
+    private String reason;
+    
+    public BotLoginEvent(BotPlayer player) {
+        this.player = player;
+    }
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+    
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+    
+    public BotPlayer getBot() {
+        return this.player;
+    }
+    
+    public String getReason() {
+        return this.reason;
+    }
+    
+    public boolean hasReason() {
+        return this.reason != null;
+    }
+    
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
